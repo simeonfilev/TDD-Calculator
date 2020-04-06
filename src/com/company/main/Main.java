6eef57347dd267e3d097f40657e233ac56d474d9
Simeon <simeontfilev@gmail.com>
2020-04-06 19:01:40 +0300

    Basic Implementation

--- src/com/company/main/Main.java
+++ src/com/company/main/Main.java
@@ -4,7 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.add(5,15));
     }
 }
